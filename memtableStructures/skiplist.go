f12c5a5523ba3ed95c8f46c6d469d01bd8b79e6e
Meris <bilalovic.meris4444@gmail.com>
2025-06-29 19:57:16 +0200

    Dodao btree.go, hashMap.go, memTable.go i skiplist.go u memtableStructures

--- memtableStructures/skiplist.go
+++ memtableStructures/skiplist.go
@@ -1,9 +1,8 @@
-package memtableStructures
+package main
 
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 type Node struct {
@@ -12,16 +11,16 @@ type Node struct {
 }
 
 type SkipList struct {
-	head      *Node
-	maxHeight int
-	height    int // trenutna visina liste
+	head        *Node
+	maxHeight   int
+	height      int // trenutna visina liste
+	lastElement *Element
 }
 
 func (s *SkipList) roll() int {
-	rand.Seed(time.Now().UnixNano())
 	level := 0
 	// Generisanje nasumicnih visina cvorova (0 ili 1)
-	for rand.Intn(2) == 1 {
+	for rand.Int31n(2) == 1 {
 		level++
 		if level >= s.maxHeight {
 			return level
@@ -34,25 +33,6 @@ func newSkipList(maxHeight int) *SkipList {
 	head := &Node{next: make([]*Node, maxHeight+1)}
 	return &SkipList{head: head, maxHeight: maxHeight, height: 0}
 }
-
-// // Funkcija za pretragu u Skip Listi
-// func (s *SkipList) search(key string) *Element {
-// 	current := s.head
-
-// 	// Krecemo od najviseg nivoa
-// 	for i := s.height; i >= 0; i-- {
-// 		for current.next[i] != nil && current.next[i].value.Key < key {
-// 			current = current.next[i]
-// 		}
-// 	}
-
-// 	current = current.next[0]
-
-// 	if current != nil && current.value.Key == key {
-// 		return current.value
-// 	}
-// 	return nil
-// }
 
 // Funkcija za umetanje u Skip Listu
 func (s *SkipList) insert(key, value string, timestamp int64, tombstone bool) {
@@ -84,41 +64,12 @@ func (s *SkipList) insert(key, value string, timestamp int64, tombstone bool) {
 		newNode.next[i] = update[i].next[i]
 		update[i].next[i] = newNode
 	}
+
+	if s.lastElement == nil || newNode.value.Timestamp > s.lastElement.Timestamp {
+		s.lastElement = &Element{Key: key, Value: value, Timestamp: timestamp, Tombstone: tombstone}
+	}
 }
 
-// // Funkcija za brisanje u Skip Listi
-// func (s *SkipList) delete(key string) {
-// 	current := s.head
-
-// 	// Niz za azuriranje pokazivaca
-// 	update := make([]*Node, s.maxHeight+1)
-
-// 	// Pretraga pozicija za azuriranje
-// 	for i := s.height; i >= 0; i-- {
-// 		for current.next[i] != nil && current.next[i].value.Key < key {
-// 			current = current.next[i]
-// 		}
-// 		update[i] = current
-// 	}
-
-// 	// Provera da li postoji novi cvor za brisanje
-// 	current = current.next[0]
-// 	if current != nil && current.value.Key == key {
-// 		for i := 0; i <= s.height; i++ {
-// 			if update[i].next[i] != current {
-// 				break
-// 			}
-// 			update[i].next[i] = current.next[i]
-// 		}
-
-// 		// Smanjivanje visine ako je poslednji nivo sada prazan
-// 		for s.height > 0 && s.head.next[s.height] == nil {
-// 			s.height--
-// 		}
-// 	}
-// }
-
-// Funkcije sa logickim brisanjem  (Na kraju dodao)
 // Funkcija za logicko brisanje u Skip Listi
 func (s *SkipList) delete(key string) {
 	current := s.head
@@ -139,10 +90,43 @@ func (s *SkipList) delete(key string) {
 	} else {
 		fmt.Printf("Kljuc %s nije pronadjen za brisanje.\n", key)
 	}
+
+	// Ako je obrisan poslednji element, pronadji novi poslednji element
+	if s.lastElement != nil && s.lastElement.Key == key {
+		s.lastElement = s.findNewLastElement()
+	}
 }
 
-// Izmenjena pretraga da ignorise logicki obrisane cvorove
-func (s *SkipList) search(key string) *Element {
+func (s *SkipList) findNewLastElement() *Element {
+	current := s.head
+	for current.next[0] != nil {
+		current = current.next[0]
+	}
+
+	// Iteriranje unazad
+	for current != s.head && current.value.Tombstone {
+		current = s.findPrevious(current)
+	}
+
+	if current == s.head {
+		return nil // Nije pronadjen validan element
+	}
+
+	return current.value
+}
+
+func (s *SkipList) findPrevious(node *Node) *Node {
+	current := s.head
+	for i := s.height; i >= 0; i-- {
+		for current.next[i] != nil && current.next[i] != node {
+			current = current.next[i]
+		}
+	}
+	return current
+}
+
+// Pretraga koja ignorise logicki obrisane cvorove
+func (s *SkipList) search(key string) (*Element, bool) {
 	current := s.head
 
 	// Pretraga od najviseg nivoa ka najnizem
@@ -156,7 +140,46 @@ func (s *SkipList) search(key string) *Element {
 
 	// Ako nadjemo cvor sa trazenim kljucem i nije logicki obrisan, vracamo ga
 	if current != nil && current.value.Key == key && !current.value.Tombstone {
-		return current.value
+		return current.value, true
 	}
-	return nil
+	return nil, false
+}
+
+// func (s *SkipList) printAll() {
+// 	current := s.head.next[0] // Pocinjemo od najnizeg nivoa
+// 	for current != nil {
+// 		if !current.value.Tombstone { // Preskacemo logicki obrisane elemente
+// 			fmt.Printf("Key: %s, Value: %s, Timestamp: %d\n", current.value.Key, current.value.Value, current.value.Timestamp)
+// 		}
+// 		current = current.next[0]
+// 	}
+// }
+
+func (s *SkipList) getAllElements() []*Element {
+	var elements []*Element
+	current := s.head.next[0] // Pocinjemo od najnizeg nivoa
+	for current != nil {
+		if !current.value.Tombstone { // Preskacemo logicki obrisane elemente
+			elements = append(elements, current.value)
+		}
+		current = current.next[0]
+	}
+	return elements
+}
+
+func (sl *SkipList) update(element Element) {
+	existingElement, found := sl.search(element.Key)
+	if found {
+		// Azuriramo vrednost ako kljuc vec postoji
+		existingElement.Value = element.Value
+		existingElement.Timestamp = element.Timestamp
+		existingElement.Tombstone = element.Tombstone
+	} else {
+		// Umecemo novi element ako kljuc ne postoji
+		sl.insert(element.Key, element.Value, element.Timestamp, element.Tombstone)
+	}
+}
+
+func (sl *SkipList) LastElement() *Element {
+	return sl.lastElement
 }
