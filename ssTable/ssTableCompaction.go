ae4eabce6be1d1a5becea743faa64858daba1288
Meris <bilalovic.meris4444@gmail.com>
2025-07-06 20:21:12 +0200

    Radne izmene na ssTableCompaction.go i ssTableCreation.go

--- ssTable/ssTableCompaction.go
+++ ssTable/ssTableCompaction.go
@@ -74,6 +74,15 @@ func initEntry(crc uint32, tombstone bool, timeStamp uint64, key []byte, value [
 		value:     value,
 	}
 }
+func InitEntry(crc uint32, tombstone bool, timeStamp uint64, key []byte, value []byte) *Entry {
+	return &Entry{
+		crc:       crc,
+		tombstone: tombstone,
+		timeStamp: timeStamp,
+		key:       key,
+		value:     value,
+	}
+}
 
 // Takes current block offset and compares it to the limit. Returns true if limit is same as offset and error if offstet > limit
 //
