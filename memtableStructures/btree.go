3111786dbe0550723af70d1703622e32727078fc
Meris <bilalovic.meris4444@gmail.com>
2025-07-26 04:19:37 +0200

    Dodavanje logickog brisanja kod b stabla

--- memtableStructures/btree.go
+++ memtableStructures/btree.go
@@ -49,40 +49,16 @@ func (treeNode *BTreeNode) findKey(k string) int {
 
 func (treeNode *BTreeNode) remove(k string) {
 	idx := treeNode.findKey(k)
-
-	// Case 1: The key to be removed is present in this node
 	if idx < treeNode.n && treeNode.keys[idx] == k {
-		if treeNode.isLeaf {
-			// If the node is a leaf node, call removeFromLeaf
-			treeNode.removeFromLeaf(idx)
-		} else {
-			// Otherwise, call removeFromNonLeaf
-			treeNode.removeFromNonLeaf(idx)
-		}
-	} else {
-		// Case 2: The key to be removed is not present in this node
-
-		// If this node is a leaf, then the key is not present in the B-Tree
-		if treeNode.isLeaf {
-			fmt.Printf("The key %s does not exist in the tree\n", k)
-			return
-		}
-
-		// Flag indicating if the key is in the last child
-		flag := (idx == treeNode.n)
-
-		// If the child where the key is supposed to exist has less than `t` keys, fill that child
-		if treeNode.children[idx].n < treeNode.degree {
-			treeNode.fill(idx)
-		}
-
-		// Recurse into the appropriate child
-		if flag && idx > treeNode.n {
-			treeNode.children[idx-1].remove(k)
-		} else {
-			treeNode.children[idx].remove(k)
-		}
+		// Logicko brisanje
+		treeNode.values[idx].Tombstone = true
+		return
 	}
+	if treeNode.isLeaf {
+		// Kljuc nije pronadjen
+		return
+	}
+	treeNode.children[idx].remove(k)
 }
 
 func (t *BTree) findNewLastElement() *Element {
@@ -103,187 +79,6 @@ func (t *BTree) findNewLastElement() *Element {
 	}
 
 	return nil // No valid element found
-}
-
-func (treeNode *BTreeNode) removeFromLeaf(idx int) {
-	// Move all keys after the idx-th position one place backward
-	for i := idx + 1; i < treeNode.n; i++ {
-		treeNode.keys[i-1] = treeNode.keys[i]
-		treeNode.values[i-1] = treeNode.values[i]
-	}
-	// Reduce the count of keys
-	treeNode.n--
-}
-
-func (treeNode *BTreeNode) removeFromNonLeaf(index int) {
-	k := treeNode.keys[index]
-
-	// Case 1: The child that precedes k (C[idx]) has at least t keys
-	if treeNode.children[index].n >= treeNode.degree {
-		pred := treeNode.getPred(index)
-		treeNode.keys[index] = pred
-		treeNode.children[index].remove(pred)
-	} else if treeNode.children[index+1].n >= treeNode.degree {
-		// Case 2: The child that succeeds k (C[idx+1]) has at least t keys
-		succ := treeNode.getSucc(index)
-		treeNode.keys[index] = succ
-		treeNode.children[index+1].remove(succ)
-	} else {
-		// Case 3: Both C[idx] and C[idx+1] have less than t keys, merge them
-		treeNode.merge(index)
-		treeNode.children[index].remove(k)
-	}
-}
-
-func (treeNode *BTreeNode) getPred(idx int) string {
-	// Start at the child preceding idx and move to the rightmost leaf node
-	cur := treeNode.children[idx]
-	for !cur.isLeaf {
-		cur = cur.children[cur.n]
-	}
-	// Return the last key in the rightmost leaf node
-	return cur.keys[cur.n-1]
-}
-
-func (treeNode *BTreeNode) getSucc(idx int) string {
-	// Start at the child following idx and move to the leftmost leaf node
-	cur := treeNode.children[idx+1]
-	for !cur.isLeaf {
-		cur = cur.children[0]
-	}
-	// Return the first key in the leftmost leaf node
-	return cur.keys[0]
-}
-
-// BorrowFromPrev borrows a key from children[index-1] and inserts it into children[index].
-func (treeNode *BTreeNode) borrowFromPrev(index int) {
-	child := treeNode.children[index]
-	sibling := treeNode.children[index-1]
-
-	// Move all keys in children[index] one step ahead.
-	for i := child.n - 1; i >= 0; i-- {
-		child.keys[i+1] = child.keys[i]
-		child.values[i+1] = child.values[i]
-	}
-
-	// If children[index] is not a leaf, move all its child pointers one step ahead.
-	if !child.isLeaf {
-		for i := child.n; i >= 0; i-- {
-			child.children[i+1] = child.children[i]
-		}
-	}
-
-	// Set the child's first key equal to keys[index-1] from the current node.
-	child.keys[0] = treeNode.keys[index-1]
-	child.values[0] = treeNode.values[index-1]
-
-	// Move sibling's last child as children[index]'s first child.
-	if !child.isLeaf {
-		child.children[0] = sibling.children[sibling.n]
-	}
-
-	// Move the key from the sibling to the parent, reducing the number of keys in the sibling.
-	treeNode.keys[index-1] = sibling.keys[sibling.n-1]
-	treeNode.values[index-1] = sibling.values[sibling.n-1]
-
-	child.n++
-	sibling.n--
-}
-
-// BorrowFromNext borrows a key from children[index+1] and places it in children[index].
-func (treeNode *BTreeNode) borrowFromNext(index int) {
-	child := treeNode.children[index]
-	sibling := treeNode.children[index+1]
-
-	// Insert keys[index] as the last key in children[index].
-	child.keys[child.n] = treeNode.keys[index]
-	child.values[child.n] = treeNode.values[index]
-
-	// Insert sibling's first child as the last child into children[index].
-	if !child.isLeaf {
-		child.children[child.n+1] = sibling.children[0]
-	}
-
-	// Set the first key of the sibling into keys[index].
-	treeNode.keys[index] = sibling.keys[0]
-	treeNode.values[index] = sibling.values[0]
-
-	// Shift all keys in sibling one step behind.
-	for i := 1; i < sibling.n; i++ {
-		sibling.keys[i-1] = sibling.keys[i]
-		sibling.values[i-1] = sibling.values[i]
-	}
-
-	// Shift all child pointers in sibling one step behind.
-	if !sibling.isLeaf {
-		for i := 1; i <= sibling.n; i++ {
-			sibling.children[i-1] = sibling.children[i]
-		}
-	}
-
-	// Update the key counts of children[index] and children[index+1].
-	child.n++
-	sibling.n--
-}
-
-// Merge merges children[index] with children[index+1] and frees children[index+1].
-func (treeNode *BTreeNode) merge(index int) {
-	child := treeNode.children[index]
-	sibling := treeNode.children[index+1]
-
-	// Pull a key from the current node and insert it into the (degree-1)th position of children[index].
-	child.keys[treeNode.degree-1] = treeNode.keys[index]
-	child.values[treeNode.degree-1] = treeNode.values[index]
-
-	// Copy the keys from children[index+1] to children[index] at the end.
-	for i := 0; i < sibling.n; i++ {
-		child.keys[i+treeNode.degree] = sibling.keys[i]
-		child.values[i+treeNode.degree] = sibling.values[i]
-	}
-
-	// Copy the child pointers from children[index+1] to children[index].
-	if !child.isLeaf {
-		for i := 0; i <= sibling.n; i++ {
-			child.children[i+treeNode.degree] = sibling.children[i]
-		}
-	}
-
-	// Shift all keys after index in the current node one step before.
-	for i := index + 1; i < treeNode.n; i++ {
-		treeNode.keys[i-1] = treeNode.keys[i]
-		treeNode.values[i-1] = treeNode.values[i]
-	}
-
-	// Shift all child pointers after (index+1) in the current node one step before.
-	for i := index + 2; i <= treeNode.n; i++ {
-		treeNode.children[i-1] = treeNode.children[i]
-	}
-
-	// Update the key count of child and the current node.
-	child.n += sibling.n + 1
-	treeNode.n--
-
-	// Free the memory occupied by sibling.
-	sibling = nil
-}
-
-func (treeNode *BTreeNode) fill(index int) {
-	// If the previous child has more than t-1 keys, borrow a key from it
-	if index != 0 && treeNode.children[index-1].n >= treeNode.degree {
-		treeNode.borrowFromPrev(index)
-	} else if index != treeNode.n && treeNode.children[index+1].n >= treeNode.degree {
-		// If the next child has more than t-1 keys, borrow a key from it
-		treeNode.borrowFromNext(index)
-	} else {
-		// Merge C[idx] with its sibling
-		// If C[idx] is the last child, merge it with its previous sibling
-		// Otherwise, merge it with its next sibling
-		if index != treeNode.n {
-			treeNode.merge(index)
-		} else {
-			treeNode.merge(index - 1)
-		}
-	}
 }
 
 func (tree *BTree) insert(element Element) {
@@ -568,6 +363,226 @@ func (bt *BTree) searchRangeInNode(node *BTreeNode, startKey, endKey string, res
 		}
 	}
 }
+
+// Fizicko brisanje
+// func (treeNode *BTreeNode) remove(k string) {
+// 	idx := treeNode.findKey(k)
+
+// 	// Case 1: The key to be removed is present in this node
+// 	if idx < treeNode.n && treeNode.keys[idx] == k {
+// 		if treeNode.isLeaf {
+// 			// If the node is a leaf node, call removeFromLeaf
+// 			treeNode.removeFromLeaf(idx)
+// 		} else {
+// 			// Otherwise, call removeFromNonLeaf
+// 			treeNode.removeFromNonLeaf(idx)
+// 		}
+// 	} else {
+// 		// Case 2: The key to be removed is not present in this node
+
+// 		// If this node is a leaf, then the key is not present in the B-Tree
+// 		if treeNode.isLeaf {
+// 			fmt.Printf("The key %s does not exist in the tree\n", k)
+// 			return
+// 		}
+
+// 		// Flag indicating if the key is in the last child
+// 		flag := (idx == treeNode.n)
+
+// 		// If the child where the key is supposed to exist has less than `t` keys, fill that child
+// 		if treeNode.children[idx].n < treeNode.degree {
+// 			treeNode.fill(idx)
+// 		}
+
+// 		// Recurse into the appropriate child
+// 		if flag && idx > treeNode.n {
+// 			treeNode.children[idx-1].remove(k)
+// 		} else {
+// 			treeNode.children[idx].remove(k)
+// 		}
+// 	}
+// }
+
+// func (treeNode *BTreeNode) removeFromLeaf(idx int) {
+// 	// Move all keys after the idx-th position one place backward
+// 	for i := idx + 1; i < treeNode.n; i++ {
+// 		treeNode.keys[i-1] = treeNode.keys[i]
+// 		treeNode.values[i-1] = treeNode.values[i]
+// 	}
+// 	// Reduce the count of keys
+// 	treeNode.n--
+// }
+
+// func (treeNode *BTreeNode) removeFromNonLeaf(index int) {
+// 	k := treeNode.keys[index]
+
+// 	// Case 1: The child that precedes k (C[idx]) has at least t keys
+// 	if treeNode.children[index].n >= treeNode.degree {
+// 		pred := treeNode.getPred(index)
+// 		treeNode.keys[index] = pred
+// 		treeNode.children[index].remove(pred)
+// 	} else if treeNode.children[index+1].n >= treeNode.degree {
+// 		// Case 2: The child that succeeds k (C[idx+1]) has at least t keys
+// 		succ := treeNode.getSucc(index)
+// 		treeNode.keys[index] = succ
+// 		treeNode.children[index+1].remove(succ)
+// 	} else {
+// 		// Case 3: Both C[idx] and C[idx+1] have less than t keys, merge them
+// 		treeNode.merge(index)
+// 		treeNode.children[index].remove(k)
+// 	}
+// }
+
+// func (treeNode *BTreeNode) getPred(idx int) string {
+// 	// Start at the child preceding idx and move to the rightmost leaf node
+// 	cur := treeNode.children[idx]
+// 	for !cur.isLeaf {
+// 		cur = cur.children[cur.n]
+// 	}
+// 	// Return the last key in the rightmost leaf node
+// 	return cur.keys[cur.n-1]
+// }
+
+// func (treeNode *BTreeNode) getSucc(idx int) string {
+// 	// Start at the child following idx and move to the leftmost leaf node
+// 	cur := treeNode.children[idx+1]
+// 	for !cur.isLeaf {
+// 		cur = cur.children[0]
+// 	}
+// 	// Return the first key in the leftmost leaf node
+// 	return cur.keys[0]
+// }
+
+// // BorrowFromPrev borrows a key from children[index-1] and inserts it into children[index].
+// func (treeNode *BTreeNode) borrowFromPrev(index int) {
+// 	child := treeNode.children[index]
+// 	sibling := treeNode.children[index-1]
+
+// 	// Move all keys in children[index] one step ahead.
+// 	for i := child.n - 1; i >= 0; i-- {
+// 		child.keys[i+1] = child.keys[i]
+// 		child.values[i+1] = child.values[i]
+// 	}
+
+// 	// If children[index] is not a leaf, move all its child pointers one step ahead.
+// 	if !child.isLeaf {
+// 		for i := child.n; i >= 0; i-- {
+// 			child.children[i+1] = child.children[i]
+// 		}
+// 	}
+
+// 	// Set the child's first key equal to keys[index-1] from the current node.
+// 	child.keys[0] = treeNode.keys[index-1]
+// 	child.values[0] = treeNode.values[index-1]
+
+// 	// Move sibling's last child as children[index]'s first child.
+// 	if !child.isLeaf {
+// 		child.children[0] = sibling.children[sibling.n]
+// 	}
+
+// 	// Move the key from the sibling to the parent, reducing the number of keys in the sibling.
+// 	treeNode.keys[index-1] = sibling.keys[sibling.n-1]
+// 	treeNode.values[index-1] = sibling.values[sibling.n-1]
+
+// 	child.n++
+// 	sibling.n--
+// }
+
+// // BorrowFromNext borrows a key from children[index+1] and places it in children[index].
+// func (treeNode *BTreeNode) borrowFromNext(index int) {
+// 	child := treeNode.children[index]
+// 	sibling := treeNode.children[index+1]
+
+// 	// Insert keys[index] as the last key in children[index].
+// 	child.keys[child.n] = treeNode.keys[index]
+// 	child.values[child.n] = treeNode.values[index]
+
+// 	// Insert sibling's first child as the last child into children[index].
+// 	if !child.isLeaf {
+// 		child.children[child.n+1] = sibling.children[0]
+// 	}
+
+// 	// Set the first key of the sibling into keys[index].
+// 	treeNode.keys[index] = sibling.keys[0]
+// 	treeNode.values[index] = sibling.values[0]
+
+// 	// Shift all keys in sibling one step behind.
+// 	for i := 1; i < sibling.n; i++ {
+// 		sibling.keys[i-1] = sibling.keys[i]
+// 		sibling.values[i-1] = sibling.values[i]
+// 	}
+
+// 	// Shift all child pointers in sibling one step behind.
+// 	if !sibling.isLeaf {
+// 		for i := 1; i <= sibling.n; i++ {
+// 			sibling.children[i-1] = sibling.children[i]
+// 		}
+// 	}
+
+// 	// Update the key counts of children[index] and children[index+1].
+// 	child.n++
+// 	sibling.n--
+// }
+
+// // Merge merges children[index] with children[index+1] and frees children[index+1].
+// func (treeNode *BTreeNode) merge(index int) {
+// 	child := treeNode.children[index]
+// 	sibling := treeNode.children[index+1]
+
+// 	// Pull a key from the current node and insert it into the (degree-1)th position of children[index].
+// 	child.keys[treeNode.degree-1] = treeNode.keys[index]
+// 	child.values[treeNode.degree-1] = treeNode.values[index]
+
+// 	// Copy the keys from children[index+1] to children[index] at the end.
+// 	for i := 0; i < sibling.n; i++ {
+// 		child.keys[i+treeNode.degree] = sibling.keys[i]
+// 		child.values[i+treeNode.degree] = sibling.values[i]
+// 	}
+
+// 	// Copy the child pointers from children[index+1] to children[index].
+// 	if !child.isLeaf {
+// 		for i := 0; i <= sibling.n; i++ {
+// 			child.children[i+treeNode.degree] = sibling.children[i]
+// 		}
+// 	}
+
+// 	// Shift all keys after index in the current node one step before.
+// 	for i := index + 1; i < treeNode.n; i++ {
+// 		treeNode.keys[i-1] = treeNode.keys[i]
+// 		treeNode.values[i-1] = treeNode.values[i]
+// 	}
+
+// 	// Shift all child pointers after (index+1) in the current node one step before.
+// 	for i := index + 2; i <= treeNode.n; i++ {
+// 		treeNode.children[i-1] = treeNode.children[i]
+// 	}
+
+// 	// Update the key count of child and the current node.
+// 	child.n += sibling.n + 1
+// 	treeNode.n--
+
+// 	// Free the memory occupied by sibling.
+// 	sibling = nil
+// }
+
+// func (treeNode *BTreeNode) fill(index int) {
+// 	// If the previous child has more than t-1 keys, borrow a key from it
+// 	if index != 0 && treeNode.children[index-1].n >= treeNode.degree {
+// 		treeNode.borrowFromPrev(index)
+// 	} else if index != treeNode.n && treeNode.children[index+1].n >= treeNode.degree {
+// 		// If the next child has more than t-1 keys, borrow a key from it
+// 		treeNode.borrowFromNext(index)
+// 	} else {
+// 		// Merge C[idx] with its sibling
+// 		// If C[idx] is the last child, merge it with its previous sibling
+// 		// Otherwise, merge it with its next sibling
+// 		if index != treeNode.n {
+// 			treeNode.merge(index)
+// 		} else {
+// 			treeNode.merge(index - 1)
+// 		}
+// 	}
+// }
 
 // func main() {
 // 	tree := newBTree(3)
